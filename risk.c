f88b1750d0f3cf070550b10eaf8b74b28030674e
Mauro Bisiani <bisianimauro@gmail.com>
2021-01-02 03:32:06 +0100

    Added time series graph

--- risk.c
+++ risk.c
@@ -10,7 +10,7 @@
 
 #define RISKS 100
 #define CYCLES 1000
-#define CYCLE_WAIT 100
+#define CYCLE_WAIT 50
 #define INITIAL_MONEY 500
 
 typedef struct player {
@@ -22,6 +22,8 @@ int minimize(int min, int max);
 int maximize(int min, int max);
 int proportionize(int min, int max);
 int minimize_ex(int min, int max);
+
+int map(int n, int domain_min, int domain_max, int range_min, int range_max);
 
 int WinMain(int argc, char **argv) {
 	if((SDL_Init(SDL_INIT_VIDEO)) != 0){
@@ -77,9 +79,10 @@ int WinMain(int argc, char **argv) {
 	// game loop
 	SDL_SetRenderDrawColor(rend, 0, 0, 0, 255);
 	SDL_RenderClear(rend);
+	SDL_RenderPresent(rend);
 	c = 0;
 	SDL_Log("Main loop: start");
-	int choice, n;
+	int choice, n, x, y;
 	while(c < CYCLES) {
 		while(SDL_PollEvent(&event)) {	// need to poll for events otherwise win becomes unresponsive
 		}
@@ -118,11 +121,33 @@ int WinMain(int argc, char **argv) {
 
 		SDL_Log("[%d] Minimizer: %d Maximizer: %d Proportional: %d Minimizer_ex: %d", c, minimizer.money, maximizer.money, proportional.money, minimizer_ex.money);
 
+		x = map(c, 0, 1000, 0, 799);
+
+		y = map(minimizer.money, -10000, 10000, 599, 0);
+		SDL_SetRenderDrawColor(rend, 255, 0, 0, 255);
+		SDL_RenderDrawPoint(rend, x, y);
+
+		y = map(maximizer.money, -10000, 10000, 599, 0);
+		SDL_SetRenderDrawColor(rend, 0, 255, 0, 255);
+		SDL_RenderDrawPoint(rend, x, y);
+
+		y = map(proportional.money, -10000, 10000, 599, 0);
+		SDL_SetRenderDrawColor(rend, 0, 0, 255, 255);
+		SDL_RenderDrawPoint(rend, x, y);
+
+		y = map(minimizer_ex.money, -10000, 10000, 599, 0);
+		SDL_SetRenderDrawColor(rend, 255, 255, 0, 255);
+		SDL_RenderDrawPoint(rend, x, y);
+
+		SDL_RenderPresent(rend);
+
 		c++;
 		SDL_Delay(CYCLE_WAIT);
 	}
 
 	SDL_Log("Main loop: end\n");
+
+	SDL_Delay(5000);
 
 	SDL_DestroyRenderer(rend);
 	SDL_DestroyWindow(win);
@@ -152,3 +177,27 @@ int proportionize(int min, int max) {
 int minimize_ex(int min, int max) {
 	return rand() % ((max - min) * 10 / 100) + min;
 }
+
+int map(int n, int domain_min, int domain_max, int range_min, int range_max) {
+	//SDL_assert(n >= domain_min && n <= domain_max);
+
+	if(n < domain_min) return range_min;
+	if(n > domain_max) return range_max;
+
+	int domain_width = domain_max - domain_min;
+	int n_dist = n - domain_min;
+	float perc = (float) n_dist / (float) domain_width;
+
+	int range_width = abs(range_max - range_min);
+	int tmp = perc * range_width;
+
+	int val = (range_max > range_min) ? range_min + tmp : range_min - tmp;
+
+	return val;
+}
+
+
+
+
+
+
