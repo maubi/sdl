10d701c575bc66efe9be9f528a37409248f21828
Mauro Bisiani <bisianimauro@gmail.com>
2021-01-02 01:49:07 +0100

    Added path traces

--- random.c
+++ random.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>
 #include <math.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #define WINDOW_WIDTH (800)
 #define WINDOW_HEIGHT (600)
+#define SKETCH_1_CYCLES 50
+#define SKETCH_2_CYCLES 10000
 
 typedef struct object {
 	float x;
 	float y;
 	float speed;
 } object_t;
+
+int monte_carlo(int min, int max);
+
+typedef struct line {
+	int x1;
+	int y1;
+	int x2;
+	int y2;
+} line_t;
 
 int WinMain(int argc, char **argv) {
 	if((SDL_Init(SDL_INIT_VIDEO)) != 0){
@@ -52,7 +64,7 @@ int WinMain(int argc, char **argv) {
 	c = 0;
 	SDL_SetRenderDrawColor(rend, 0, 0, 0, 255);
 	SDL_RenderClear(rend);
-	while(c < 30) {
+	while(c < SKETCH_1_CYCLES) {
 		while(SDL_PollEvent(&event)) {	// need to poll for events otherwise win becomes unresponsive
 		}
 
@@ -83,28 +95,48 @@ int WinMain(int argc, char **argv) {
 	rect.x = (WINDOW_WIDTH - 50) / 2;
 	rect.y = (WINDOW_HEIGHT - 50) / 2;
 	rect.w = rect.h = 50;
-	while(c < 30) {
+	line_t lines[SKETCH_2_CYCLES];
+	while(c < SKETCH_2_CYCLES) {
 		while(SDL_PollEvent(&event)) {		// need to poll the event queue otherwise win becomes unresponsive
 		}
+
+		SDL_Log("Cycle: %d", c);
 
 		SDL_SetRenderDrawColor(rend, 0, 0, 0, 255);
 		SDL_RenderClear(rend);
 		
-		step_x = (rand() % 3) -1;
-		step_y = (rand() % 3) -1;
+		step_x = (rand() % 3) -1;	// choose a random direction
+		step_y = (rand() % 3) -1;	// choose a random direction
 
-		step_x *= monte_carlo(0, 20);
-		step_y *= monte_carlo(0, 20);
+		step_x *= monte_carlo(0, 70);	// choose a random length
+		step_y *= monte_carlo(0, 70);	// choose a random length
 
-		rect.x += step_x;
-		rect.y += step_y;
+		int new_x = rect.x + step_x;
+		int new_y = rect.y + step_y;
+		new_x = (new_x > WINDOW_WIDTH - 50) ? WINDOW_WIDTH - 50 : new_x; 
+		new_x = (new_x < 0) ? 0 : new_x; 
+		new_y = (new_y > WINDOW_HEIGHT - 50) ? WINDOW_HEIGHT - 50 : new_y; 
+		new_y = (new_y < 0) ? 0 : new_y; 
+
+		lines[c].x1 = rect.x;
+		lines[c].y1 = rect.y;
+		lines[c].x2 = new_x;
+		lines[c].y2 = new_y;
+
+		SDL_SetRenderDrawColor(rend, 255, 0, 0, 255);
+		for(int i = 0; i <= c; i++) {
+			SDL_RenderDrawLine(rend, lines[i].x1, lines[i].y1, lines[i].x2, lines[i].y2);
+		}
+
+		rect.x = new_x;
+		rect.y = new_y;
 
 		SDL_SetRenderDrawColor(rend, 30, 240, 128, 255);
 		SDL_RenderDrawRect(rend, &rect);
 		SDL_RenderPresent(rend);
 		
 		c++;
-		SDL_Delay(500);
+		SDL_Delay(100);
 	}
 
 	printf("Bye!\n");
@@ -120,7 +152,7 @@ int monte_carlo(int min, int max) {
 	while(true) {
 		r1 = (rand() % max) + min;
 		r2 = (rand() % max) + min;
-		if(r2 < r1) {
+		if(r2 > r1) {
 			return r1;
 		}
 	}
